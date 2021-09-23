a469680cecd3fe86067ed0c462dc782f62717bfa
David Campos da Cruz <70824817+David-Campos-maker@users.noreply.github.com>
2021-09-22 22:04:36 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from Lista import Lista
+
+minhaLista = Lista()
+
+minhaLista.insert(minhaLista , 1)
+minhaLista.insert(minhaLista , 2)
+minhaLista.insert(minhaLista , 3)
+minhaLista.insert(minhaLista , 4)
+minhaLista.insert(minhaLista , 5)
+
+minhaLista.print_lista(minhaLista)
+
+minhaLista.remove(minhaLista)
+
+minhaLista.print_lista(minhaLista)
